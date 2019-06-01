b07970e11588f8473b0f832702286be8bcab38cf
Dmitry <dmiandich@yandex.ru>
2019-06-01 11:46:55 +0300

    Avoid innerHTML to meet AMO requirements

--- expimp.js
+++ expimp.js
@@ -193,7 +193,7 @@ function importParcedData(datparced)
     }
     var eventbkgrnd = document.getElementById("dataimported");
     eventbkgrnd.style.display = "block";
-    document.getElementById("eventmessage").innerHTML = "Осуществлен импорт данных CONText.<br>" + "Загружено:<br>" + numranks + " статусов,<br>" + numusers + " событий истории.";
+    document.getElementById("eventmessage").innerText = "Осуществлен импорт данных CONText.\n Загружено: \n" + numranks + " статусов,\n" + numusers + " событий истории	.";
     
     var closebtn = document.getElementsByClassName("close")[0];
     closebtn.onclick = function() {  eventbkgrnd.style.display = "none"; }
